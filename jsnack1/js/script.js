0b7e430ac5e98249bab49630c4067c19f04a7539
jacopo35 <jaczor00@gmail.com>
2021-11-26 15:46:12 +0100

    add-first-snack

---
+++ jsnack1/js/script.js
@@ -0,0 +1,13 @@
+/* Jsnack1
+Il software deve chiedere per 10 volte all’utente di inserire un numero.Il programma stampa la somma di tutti i numeri inseriti.*/
+
+
+
+let somma = 0;
+let i = 0;
+while (i < 10) {
+    let numero = prompt('Inserisci un numero');
+    somma += parseInt(numero);
+    i++;
+}
+console.log(somma);
