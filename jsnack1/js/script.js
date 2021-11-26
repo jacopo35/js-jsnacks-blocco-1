7b358e719a7f5bf271b12f355a69739ee71ee0e7
jacopo35 <jaczor00@gmail.com>
2021-11-26 15:56:10 +0100

    add-html-snack2

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,5 +1,6 @@
 /* Jsnack1
-Il software deve chiedere per 10 volte all’utente di inserire un numero.Il programma stampa la somma di tutti i numeri inseriti.*/
+Il software deve chiedere per 10 volte all’utente di inserire un numero.Il programma stampa la somma di tutti i numeri inseriti.
+*/
 
 
 
